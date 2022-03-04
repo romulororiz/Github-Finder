07d1c7612b7ab10cb91f312106ce41e705b6f243
Romulo Nunes de Menezes Roriz <romulonmroriz@gmail.com>
2022-03-04 23:45:15 +0100

    Setting up context

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,13 @@
+function Spinner() {
+	return (
+		<div className='sk-chase mt-20 w-100 mx-auto text-center'>
+			<div className='sk-chase-dot'></div>
+			<div className='sk-chase-dot'></div>
+			<div className='sk-chase-dot'></div>
+			<div className='sk-chase-dot'></div>
+			<div className='sk-chase-dot'></div>
+			<div className='sk-chase-dot'></div>
+		</div>
+	);
+}
+export default Spinner;
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
 	const [users, setUsers] = useState([]);
 	const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		fetchUsers();
-	}, []);
-
 	const fetchUsers = async () => {
-		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}users`, {
+		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
 			headers: {
 				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
 			},
@@ -17,16 +15,26 @@ function UserResults() {
 
 		const data = await response.json();
 
+		console.log(data);
+
 		setUsers(data);
 		setLoading(false);
 	};
 
-	return (
-		<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols 3 md:grid-cols-2'>
-			{users.map(user => (
-				<h3 key={user.id}>{user.login}</h3>
-			))}
-		</div>
-	);
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	if (!loading) {
+		return (
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols 3 md:grid-cols-2'>
+				{users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
+	} else {
+		return <Spinner />;
+	}
 }
 export default UserResults;
