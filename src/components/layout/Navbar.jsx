0cf820994ed93418ea8d74c4a78329d778c193de
Romulo Nunes de Menezes Roriz <romulonmroriz@gmail.com>
2022-03-04 14:27:36 +0100

    Created UserResults component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Navbar({title}) {
+function Navbar({ title }) {
 	return (
 		<nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
 			<div className='container mx-auto'>
@@ -12,12 +12,16 @@ function Navbar({title}) {
 						{title}
 					</Link>
 				</div>
-        <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
-            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>Home</Link>
-            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>About</Link>
-          </div>
-        </div>
+				<div className='flex-1 px-2 mx-2'>
+					<div className='flex justify-end'>
+						<Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+							Home
+						</Link>
+						<Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+							About
+						</Link>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
